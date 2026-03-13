e44e389a4a2aedab3c09b5f9c0c309d18fc6f305
Chithra123-byte <chithra18ardi@gmail.com>
2026-03-13 12:09:48 +0530

    updated project

--- eg2/app.py
+++ eg2/app.py
@@ -6,19 +6,24 @@ from pymongo import MongoClient
 client=MongoClient("mongodb://localhost:27017/")
 db=client["college"]
 table=db["SIT"]
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])#program starts from here
 def home():
-    return render_template("homepage.html")#fetching the homepage.html file from templates folder
+    return render_template("student.html")#fetching the homepage.html file from templates folder
 @app.route(rule="/con",methods=["GET","POST"])#insert data into the database
 def dbcon():
-    fname=" "
-    fusn=" "
-    sem=" "
-    if request.method == "POST":
         fname = request.form.get("name")
         fusn = request.form.get("usn")
         sem = request.form.get("sem")
-        data={"name":fname,"usn":fusn,"sem":sem}
-        db.table.insert_one(data)
+        marks = int(request.form["marks"])
+        data={"name":fname,"usn":fusn,"sem":sem,"marks":marks}
+        table.insert_one(data)
         return "data inserted successfully"
+@app.route("/studentInfo")
+def studentInfo():
+    info=table.find({"marks":{"$gt":25}})
+    return render_template("show.html",students=info)#passing the data to studentInfo.html file
+
+
+
+
 app.run(debug=True)
